782336f50b7a2aa94f6938f0be402a6e80918e8e
Omar Vazquez <omar.vazquezac@gmail.com>
2016-12-21 10:54:28 -0600

    CORS

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 module.exports = function (app) {
   const ezTable = require('./ez-table')
 
-  router.use(function (req, res, next) {
+  app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
